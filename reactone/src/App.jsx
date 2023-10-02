3c2cc6c369155bb2253bfee6ad7411b0b4b9d2d6
Anand14879 <np01cp4a210042@islingtoncollege.edu.np>
2023-10-02 12:56:41 +0545

    Another commit

--- reactone/src/App.jsx
+++ reactone/src/App.jsx
@@ -1,7 +1,9 @@
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
-import Map1 from "./components/Map/Map";
+import Map1 from "./components/Map/Map1";
+// import Map from "./components/Map/Map";
 import { getPlacesData } from "./api/index.jsx";
+// import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 import "./App.css";
 import { useEffect, useState } from "react";
 
@@ -10,25 +12,23 @@ import { CssBaseline, Grid } from "@material-ui/core";
 export default function App() {
     const [places, setPlaces] = useState([]);
 
-    const [coordinates, setCoordinates] = useState([]);
+    const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState(null);
-
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(
             ({ coords: { latitude, longitude } }) => {
-                setCoordinates([latitude, longitude]);
+                console.log(latitude, longitude);
+                setCoordinates({ lat: latitude, lng: longitude });
             }
         );
     }, []);
 
-    useEffect(() => {
-        if (bounds) {
-            getPlacesData(bounds.sw, bounds.ne).then((data) => {
-                console.log(data);
-                setPlaces(data);
-            });
-        }
-    }, [coordinates, bounds]);
+    // useEffect(() => {
+    //     getPlacesData(bounds.sw, bounds.ne).then((data) => {
+    //         console.log(data);
+    //         setPlaces(data);
+    //     });
+    // }, [coordinates, bounds]);
 
     return (
         <>
@@ -39,6 +39,7 @@ export default function App() {
                     <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
+                    {/* <Map /> */}
                     <Map1
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
